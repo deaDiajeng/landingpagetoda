6fb5adb65a61a138997c57b8df894bbaaa835904
M4tchaa <arlialya@gmail.com>
2024-05-03 23:09:31 +0700

    no msg

--- index.php
+++ index.php
@@ -15,7 +15,8 @@
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
         <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
         <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
-        <link href="assets/css/bootstrap.css?v=<?=$custom_css?>">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+        <!-- <link href="assets/css/bootstrap.css?v=<?=$custom_css?>"> -->
     </head>
     <body id="page-top">
         <?php
@@ -24,10 +25,8 @@
         include('content/section.php');
         ?>
         
-        <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
         <script src="assets/js/bootstrap.js?v=<?=$custom_js?>"></script>
-        <script src="https://cdn./bootstrap.min.js"></script>
     </body>
-</html>startbootstrap.com/sb-forms-latest.js"></script> -->
-        <script src="<?= BASEURL; ?>/js/bootstrap.js"></script>
-        <script src="<?= BASEURL; ?>/js
+</html>
