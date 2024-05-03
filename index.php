2ebd9bf54d8aca0eba25304908ab5a93568ee703
deaDiajeng <168566666+deaDiajeng@users.noreply.github.com>
2024-05-03 15:37:31 +0700

    login udh ada, tampilan msh acak2an

---
+++ index.php
@@ -0,0 +1,33 @@
+<!-- <?php
+  $custom_css=filemtime("css/styles.css");
+  $custom_js=filemtime("js/scripts.js");
+  ?> -->
+  
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Rumah Quran Insan Toda</title>
+        <link rel="icon" type="image/x-icon" href="assets/rq.png" />
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
+        <link href="assets/css/bootstrap.css?v=<?=$custom_css?>">
+    </head>
+    <body id="page-top">
+        <?php
+        include('content/navbar.php');
+        include('content/header.php');
+        include('content/section.php');
+        ?>
+        
+        <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/js/bootstrap.js?v=<?=$custom_js?>"></script>
+        <script src="https://cdn./bootstrap.min.js"></script>
+    </body>
+</html>startbootstrap.com/sb-forms-latest.js"></script> -->
+        <script src="<?= BASEURL; ?>/js/bootstrap.js"></script>
+        <script src="<?= BASEURL; ?>/js
